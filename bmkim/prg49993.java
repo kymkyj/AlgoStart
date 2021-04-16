a6f01a205c3042bd14026a64389e9283bb3a09c8
bomingeem <bomingeem@gmail.com>
2021-04-16 17:28:51 +0900

    [fix] 스킬트리: 프로그래머스 문제 유형 형태로 수정

--- bmkim/prg49993.java
+++ bmkim/prg49993.java
@@ -3,22 +3,29 @@ package study.programmers.level2;
 public class prg49993 {
     public static void main(String[] args) {
         //스킬트리
-        int answer = 0;
         String skill = "CBD";
         String[] skill_trees = {"BACDE", "CBADF", "AECB", "BDA"};
+        Solution solution = new Solution();
+
+        System.out.println(solution.solution(skill, skill_trees));
+    }
+}
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
 
         int index = 0;
         while (true) {
             String skill_tree = skill_trees[index];
-            String skill_tree_temp = skill_tree;
+            String skill_tree_clone = new String(skill_tree);
             int size = skill_tree.length();
 
             for(int i=0; i<size; i++) {
-                String charSkill = String.valueOf(skill_tree_temp.charAt(i));
+                String charSkill = String.valueOf(skill_tree_clone.charAt(i));
                 if (!skill.contains(charSkill)) {
                     skill_tree = skill_tree.replace(charSkill, "");
                 }
-
             }
             if (skill.indexOf(skill_tree) == 0) {
                 answer++;
@@ -26,6 +33,6 @@ public class prg49993 {
             index++;
             if (index == skill_trees.length) break;
         }
-        System.out.println(answer);
+        return answer;
     }
 }
