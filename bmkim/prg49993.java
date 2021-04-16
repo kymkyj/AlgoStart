02201b8a8cb39af2453ee5bbc79da34fd4bfc50c
bomingeem <bomingeem@gmail.com>
2021-04-16 17:33:22 +0900

    [fix] 스킬트리: inner class로 변경

--- bmkim/prg49993.java
+++ bmkim/prg49993.java
@@ -9,30 +9,32 @@ public class prg49993 {
 
         System.out.println(solution.solution(skill, skill_trees));
     }
-}
 
-class Solution {
-    public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
+    static class Solution {
+        public int solution(String skill, String[] skill_trees) {
+            int answer = 0;
 
-        int index = 0;
-        while (true) {
-            String skill_tree = skill_trees[index];
-            String skill_tree_clone = new String(skill_tree);
-            int size = skill_tree.length();
+            int index = 0;
+            while (true) {
+                String skill_tree = skill_trees[index];
+                String skill_tree_clone = new String(skill_tree);
+                int size = skill_tree.length();
 
-            for(int i=0; i<size; i++) {
-                String charSkill = String.valueOf(skill_tree_clone.charAt(i));
-                if (!skill.contains(charSkill)) {
-                    skill_tree = skill_tree.replace(charSkill, "");
+                for(int i=0; i<size; i++) {
+                    String charSkill = String.valueOf(skill_tree_clone.charAt(i));
+                    if (!skill.contains(charSkill)) {
+                        skill_tree = skill_tree.replace(charSkill, "");
+                    }
                 }
+                if (skill.indexOf(skill_tree) == 0) {
+                    answer++;
+                }
+                index++;
+                if (index == skill_trees.length) break;
             }
-            if (skill.indexOf(skill_tree) == 0) {
-                answer++;
-            }
-            index++;
-            if (index == skill_trees.length) break;
+            return answer;
         }
-        return answer;
     }
 }
+
+
